77c5472f4e1b83d536b4654973d0c11ced6e57bb
Rafael Acevedo <raa7@cin.ufpe.br>
2016-05-21 11:49:22 -0300

    Add login handling

--- src/br/ufpe/cin/projetop2/application/Application.java
+++ src/br/ufpe/cin/projetop2/application/Application.java
@@ -1,10 +1,24 @@
 package br.ufpe.cin.projetop2.application;
 
+import br.ufpe.cin.projetop2.application.console.login.LoginHandler;
+
 public class Application {
 
-	public static void main(String[] args) {
+	private static void login() {
+		LoginHandler loginHandler = new LoginHandler();
+		while(!loginHandler.login()) {
+			System.out.println("Login failed. Please try again");
+		}
 		
-
+		System.out.println("Welcome to our application");
+		 
 	}
+	
+	public static void main(String[] args) {
+		login();
+		 
+	}
+	
+	
 
 }
---
+++ src/br/ufpe/cin/projetop2/application/console/login/LoginHandler.java
@@ -0,0 +1,29 @@
+package br.ufpe.cin.projetop2.application.console.login;
+
+import java.util.HashMap;
+
+import br.ufpe.cin.projetop2.application.console.ConsoleHandler;
+
+public class LoginHandler extends ConsoleHandler{
+	
+	private HashMap<String, String> passwords;
+	public LoginHandler() {
+		this.passwords = new HashMap<>();
+		this.passwords.put("admin", "admin");
+	}
+	
+	public boolean login() {
+		String username = this.getUsername();
+		String password = this.getPassword();
+		String storedPassword = this.passwords.get(username);
+		return storedPassword != null && storedPassword.equals(password);
+	}
+	
+	private String getUsername() {
+		return this.getConsoleString("Type in your username");
+	}
+	
+	private String getPassword() {
+		return this.getConsoleString("Type in your password:");
+	}
+}
