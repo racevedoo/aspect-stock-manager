9c41cb575a358b3260515ce5fc41bcbdb26bb957
Rafael Acevedo <raa7@cin.ufpe.br>
2016-06-12 07:17:11 -0300

    Revert "Initial interface design"
    This reverts commit 187bc8808090a0df929d0b2aa018223faabf881a, reversing
    changes made to 73ce1d40622b3f5cde63c758a55f6aca825c407f.

--- src/br/ufpe/cin/projetop2/application/console/ConsoleHandler.java
+++ src/br/ufpe/cin/projetop2/application/console/ConsoleHandler.java
@@ -3,19 +3,12 @@ package br.ufpe.cin.projetop2.application.console;
 import java.util.Scanner;
 
 public class ConsoleHandler {
-<<<<<<< HEAD
   private static Scanner in = null; 
 
   public void open() {
     if (in == null) {
       in = new Scanner(System.in);
     }
-=======
-  private static Scanner in; 
-
-  public void open() {
-    in = new Scanner(System.in);
->>>>>>> 9ef4ad4873afee73c4d3cb4bf1e45d587702ed19
   }
   public String getConsoleString(String msg) {
     System.out.println(msg);
--- src/br/ufpe/cin/projetop2/aspects/ConsoleAspect.aj
+++ src/br/ufpe/cin/projetop2/aspects/ConsoleAspect.aj
@@ -6,10 +6,6 @@ public aspect ConsoleAspect {
   Object around(ConsoleHandler instance) : execution(* ConsoleHandler.getConsole*(..)) && this(instance){
     instance.open();
     Object ret = proceed(instance);
-<<<<<<< HEAD
-=======
-    instance.close();
->>>>>>> 9ef4ad4873afee73c4d3cb4bf1e45d587702ed19
     return ret;
   }
 }
